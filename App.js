53b9d2ff6dc8b6fed46d4a91df18ff9212de6333
Shashank <shashankdewangan1000@gmail.com>
2021-08-30 14:30:12 +0530

    deployed my code

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import * as Contacts from "expo-contacts";
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+
+      if (status === "granted") {
+        const data = await Contacts.getContactsAsync();
+        //console.log(JSON.stringify(data));  
+        for (var i=0;i<data.data.length;i++){
+          
+          var name = data.data[i].name
+          for(var j=0; j<data.data[i].phoneNumbers.length;j++){
+            
+              var no = data.data[i].phoneNumbers[j].number;
+              console.log(name,no);
+
+          }
+
+        }
+        
+        
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Contacts displayed</Text>
     </View>
   );
 }
@@ -14,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
